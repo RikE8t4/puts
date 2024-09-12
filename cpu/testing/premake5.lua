7cef8d1c5a09d42b5b549a3c71a0da799d8187c1
RikE4 <148019460+RikE4@users.noreply.github.com>
2024-09-12 00:52:30 -0300

    sabe
    sabfg

---
+++ cpu/testing/premake5.lua
@@ -0,0 +1,27 @@
+project_root = "../../../.."
+include(project_root.."/tools/build")
+
+test_suite("xenia-cpu-tests", project_root, ".", {
+  links = {
+    "capstone",
+    "fmt",
+    "imgui",
+    "xenia-base",
+    "xenia-core",
+    "xenia-cpu",
+    "xenia-gpu",
+
+    -- TODO(benvanik): cut these dependencies?
+    "xenia-kernel",
+    "xenia-ui", -- needed by xenia-base
+    "xenia-patcher",
+  },
+  filtered_links = {
+    {
+      filter = 'architecture:x86_64',
+      links = {
+        "xenia-cpu-backend-x64",
+      },
+    }
+  },
+})
